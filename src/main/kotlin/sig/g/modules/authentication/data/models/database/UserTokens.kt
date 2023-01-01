84beaad080d43c800d49fbbe2d8e0b2da1d2c695
milky <georgesg97@gmail.com>
2023-01-01 18:29:33 +0000

    GS-27 oauth registration/login sign in

--- src/main/kotlin/sig/g/modules/authentication/data/models/database/UserTokens.kt
+++ src/main/kotlin/sig/g/modules/authentication/data/models/database/UserTokens.kt
@@ -7,8 +7,6 @@ object UserTokens : Table() {
     val userTokenId = uuid("userTokenId").autoGenerate()
     val userId = text("userId").references(Users.userId)
     val accessToken = uuid("accessToken")
-    val refreshToken = uuid("refreshToken")
-    val expiresAt = datetime("expiresAt")
 
     override val primaryKey: PrimaryKey = PrimaryKey(userTokenId)
 }
