831d8aec3ccb0a6671fc1257c92171fc1a648acc
milky <georgesg97@gmail.com>
2023-01-03 23:41:30 +0000

    generic response

---
+++ src/main/kotlin/uk/co/culturebook/data_access/models/GenericResponse.kt
@@ -0,0 +1,6 @@
+package uk.co.culturebook.data_access.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GenericResponse(val success: Boolean)
--- src/main/kotlin/uk/co/culturebook/modules/authentication/routes/general/ForgotPasswordRoute.kt
+++ src/main/kotlin/uk/co/culturebook/modules/authentication/routes/general/ForgotPasswordRoute.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import uk.co.culturebook.data_access.models.GenericResponse
 import uk.co.culturebook.modules.authentication.constants.AuthRoute
 import uk.co.culturebook.modules.authentication.logic.forgotPassword
 import uk.co.culturebook.modules.authentication.logic.resetPassword
@@ -14,14 +15,13 @@ import uk.co.culturebook.modules.email.data.PasswordResetRequest
 internal fun Route.resetPassword() {
     post(AuthRoute.ResetPassword.route) {
         val passwordReset = call.receive<PasswordReset>()
-        if (resetPassword(passwordReset)) call.respond(HttpStatusCode.OK) else call.respond(HttpStatusCode.BadRequest)
+        if (resetPassword(passwordReset)) call.respond(HttpStatusCode.OK, GenericResponse(true)) else call.respond(HttpStatusCode.BadRequest)
     }
 }
 
 internal fun Route.forgotPasswordRoute() {
     post(AuthRoute.ForgotPassword.route) {
         val passwordResetRequest = call.receive<PasswordResetRequest>()
-        forgotPassword(passwordResetRequest)
-        call.respond(HttpStatusCode.OK)
+        call.respond(HttpStatusCode.OK, GenericResponse(forgotPassword(passwordResetRequest)))
     }
 }
