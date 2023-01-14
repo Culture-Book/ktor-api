7bf6ba514c838e796843ed8ab1dd04cfdb53e4a3
milky <georgesg97@gmail.com>
2023-01-14 15:54:19 +0000

    GS-40 Fixed postgres not understanding DISTANCE_IN_KM

--- src/main/kotlin/uk/co/culturebook/modules/database/DatabaseFuncs.kt
+++ src/main/kotlin/uk/co/culturebook/modules/database/DatabaseFuncs.kt
@@ -9,14 +9,14 @@ internal fun Transaction.getDistanceFunction() {
             """
                 CREATE OR REPLACE FUNCTION DISTANCE_IN_KM(lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION, lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION)
                 RETURNS DOUBLE PRECISION AS ${'$'}${'$'}
-                    SELECT ST_Distance_Sphere(ST_MakePoint(lon1, lat1), ST_MakePoint(lon2, lat2))/1000;
+                    SELECT (6371 * acos(cos(radians(lat1)) * cos(radians(lat2)) * cos(radians(lon2) - radians(lon1)) + sin(radians(lat1)) * sin(radians(lat2)))) AS distance
                 ${'$'}${'$'} LANGUAGE SQL;
             """.trimIndent()
         )
 
         else -> exec(
             """
-                CREATE FUNCTION DISTANCE_IN_KM(lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION, lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION)
+                CREATE FUNCTION IF NOT EXISTS DISTANCE_IN_KM(lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION, lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION)
                 RETURNS DOUBLE PRECISION AS ${'$'}${'$'}
                     SELECT (6371 * acos(cos(radians(lat1)) * cos(radians(lat2)) * cos(radians(lon2) - radians(lon1)) + sin(radians(lat1)) * sin(radians(lat2)))) AS distance
                 ${'$'}${'$'} LANGUAGE SQL;
