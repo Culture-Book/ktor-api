11517dff0a091f7c95106ade793db3069ff3bea2
milky <georgesg97@gmail.com>
2023-03-14 14:08:14 +0000

    Fix linked elements

--- src/main/kotlin/uk/co/culturebook/modules/culture/data/database/tables/contribution/LinkedContributions.kt
+++ src/main/kotlin/uk/co/culturebook/modules/culture/data/database/tables/contribution/LinkedContributions.kt
@@ -2,10 +2,11 @@ package uk.co.culturebook.modules.culture.data.database.tables.contribution
 
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
+import uk.co.culturebook.modules.culture.data.database.tables.element.Elements
 
 object LinkedContributions : Table() {
     val parent_element_id = uuid("parent_element_id").autoGenerate()
-        .references(Contributions.id, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
+        .references(Elements.id, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
     val child_element_id = uuid("child_element_id").autoGenerate()
-        .references(Contributions.id, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
+        .references(Elements.id, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
 }
