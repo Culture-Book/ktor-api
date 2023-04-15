f59558066f455e486643b06b5d87ddf6762069e9
milky <georgesg97@gmail.com>
2023-04-15 22:58:24 +0100

    move assets to cloud

--- src/main/kotlin/uk/co/culturebook/modules/authentication/data/AuthConfig.kt
+++ src/main/kotlin/uk/co/culturebook/modules/authentication/data/AuthConfig.kt
@@ -5,6 +5,9 @@ import io.ktor.server.config.*
 object AuthConfig {
     val ApplicationConfig.toSDate get() = property("ktor.generic.tos_date").getString()
     val ApplicationConfig.privacyDate get() = property("ktor.generic.privacy_date").getString()
+    val ApplicationConfig.toSLink get() = property("ktor.generic.tos_link").getString()
+    val ApplicationConfig.privacyLink get() = property("ktor.generic.privacy_link").getString()
+    val ApplicationConfig.assetLink get() = property("ktor.generic.asset_link").getString()
     val ApplicationConfig.issuer get() = property("ktor.jwt.issuer").getString()
     val ApplicationConfig.realm get() = property("ktor.jwt.realm").getString()
     val ApplicationConfig.jwtExpiry get() = property("ktor.jwt.accessTokenExpiry").getString()
--- src/main/kotlin/uk/co/culturebook/modules/authentication/routes/resources/Static.kt
+++ src/main/kotlin/uk/co/culturebook/modules/authentication/routes/resources/Static.kt
@@ -1,28 +1,39 @@
 package uk.co.culturebook.modules.authentication.routes.resources
 
+import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import uk.co.culturebook.Constants.WellKnownRoute
+import uk.co.culturebook.modules.authentication.data.AuthConfig.assetLink
+import uk.co.culturebook.modules.authentication.data.AuthConfig.privacyLink
+import uk.co.culturebook.modules.authentication.data.AuthConfig.toSLink
 import uk.co.culturebook.modules.authentication.data.interfaces.AuthRoute
 
 
 internal fun Route.getTos() {
     static(AuthRoute.User.Tos.route) {
-        staticBasePackage = "docs"
-        resource("EULA.html")
+        get("EULA.html") {
+            val tosLink = call.application.environment.config.toSLink
+            call.respondRedirect(tosLink, true)
+        }
     }
 }
 
 internal fun Route.getPrivacy() {
     static(AuthRoute.User.Privacy.route) {
-        staticBasePackage = "docs"
-        resource("Privacy_Notice.html")
+        get("Privacy_Notice.html") {
+            val privacyLink = call.application.environment.config.privacyLink
+            call.respondRedirect(privacyLink, true)
+        }
     }
 }
 
 internal fun Route.getAssetLinks() {
     static(WellKnownRoute) {
-        staticBasePackage = "assets"
-        resource("assetlinks.json")
+        get("assetlinks.json") {
+            val assetLink = call.application.environment.config.assetLink
+            call.respondRedirect(assetLink, true)
+        }
     }
 }
